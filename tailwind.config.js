5556d01bb2568ac1d9e70afe3c2671ce8d848e47
Vadim Savin <savinvadim1312@gmail.com>
2024-09-06 15:18:53 +0200

    Initial commit
    Generated by create-expo-stack 2.11.24

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
+
+  presets: [require('nativewind/preset')],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
