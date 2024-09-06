438f4b75014cce7d0b8653cfacf410838a38f8a6
Vadim Savin <savinvadim1312@gmail.com>
2024-09-06 16:52:05 +0200

    RSPV footer

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,6 +1,6 @@
 import dayjs from 'dayjs';
 import { useLocalSearchParams, Stack } from 'expo-router';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Pressable } from 'react-native';
 
 import events from '~/assets/events.json';
 
@@ -31,6 +31,15 @@ export default function EventPage() {
       <Text className="text-lg" numberOfLines={2}>
         {event.description}
       </Text>
+
+      {/* Footer */}
+      <View className="absolute bottom-0 left-0 right-0 flex-row items-center justify-between border-t-2 border-gray-300 p-5 pb-10">
+        <Text className="text-xl font-semibold">Free</Text>
+
+        <Pressable className="rounded-md bg-red-500 p-5 px-8">
+          <Text className="text-lg font-bold text-white">Join and RSVP</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
