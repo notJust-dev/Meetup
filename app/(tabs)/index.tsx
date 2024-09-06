da96112d38eef4526a116e16999d75ce92f296a4
Vadim Savin <savinvadim1312@gmail.com>
2024-09-06 16:09:38 +0200

    Event list item, styles with nativewind, custom component, props

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,19 @@
+import Feather from '@expo/vector-icons/Feather';
 import { Stack } from 'expo-router';
-import { View } from 'react-native';
+import { View, Text, Image } from 'react-native';
+
+import events from '~/assets/events.json';
+import EventListItem from '~/components/EventListItem';
 
 export default function Events() {
   return (
     <>
       <Stack.Screen options={{ title: 'Events' }} />
+
+      {/* Event list item */}
+      <EventListItem event={events[0]} />
+      <EventListItem event={events[1]} />
+      <EventListItem event={events[2]} />
     </>
   );
 }
---
+++ components/EventListItem.tsx
@@ -0,0 +1,32 @@
+import Feather from '@expo/vector-icons/Feather';
+import { View, Text, Image } from 'react-native';
+
+export default function EventListItem({ event }) {
+  return (
+    <View className="gap-3 p-3">
+      <View className="flex-row">
+        <View className="flex-1 gap-2">
+          <Text className="text-lg font-semibold uppercase text-amber-800">
+            Wed 13, Sep · 19:30 CET
+          </Text>
+          <Text className="text-xl font-bold" numberOfLines={2}>
+            {event.title}
+          </Text>
+
+          <Text className="text-gray-700">{event.location}</Text>
+        </View>
+
+        {/* Event image */}
+        <Image source={{ uri: event.image }} className="aspect-video w-2/5 rounded-xl" />
+      </View>
+
+      {/* Footer */}
+      <View className="flex-row gap-3">
+        <Text className="mr-auto text-gray-700">16 going</Text>
+
+        <Feather name="share" size={20} color="gray" />
+        <Feather name="bookmark" size={24} color="gray" />
+      </View>
+    </View>
+  );
+}
