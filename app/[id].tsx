c92369f4a931778a8f2081035c2c792f9e23085c
Vadim Savin <savinvadim1312@gmail.com>
2024-09-06 18:58:19 +0200

    Attendance RSVP

--- app/[id].tsx
+++ app/[id].tsx
@@ -3,13 +3,17 @@ import { useLocalSearchParams, Stack } from 'expo-router';
 import { useEffect, useState } from 'react';
 import { Text, View, Image, Pressable, ActivityIndicator } from 'react-native';
 
+import { useAuth } from '~/contexts/AuthProvider';
 import { supabase } from '~/utils/supabase';
 
 export default function EventPage() {
   const { id } = useLocalSearchParams();
 
   const [event, setEvent] = useState(null);
+  const [attendace, setAttendance] = useState(null);
   const [loading, setLoading] = useState(false);
+
+  const { user } = useAuth();
 
   useEffect(() => {
     fetchEvent();
@@ -19,7 +23,26 @@ export default function EventPage() {
     setLoading(true);
     const { data, error } = await supabase.from('events').select('*').eq('id', id).single();
     setEvent(data);
+
+    const { data: attendanceData } = await supabase
+      .from('attendance')
+      .select('*')
+      .eq('user_id', user.id)
+      .eq('event_id', id)
+      .single();
+    setAttendance(attendanceData);
+
     setLoading(false);
+  };
+
+  const joinEvent = async () => {
+    const { data, error } = await supabase
+      .from('attendance')
+      .insert({ user_id: user.id, event_id: event.id })
+      .select()
+      .single();
+
+    setAttendance(data);
   };
 
   if (loading) {
@@ -53,9 +76,13 @@ export default function EventPage() {
       <View className="absolute bottom-0 left-0 right-0 flex-row items-center justify-between border-t-2 border-gray-300 p-5 pb-10">
         <Text className="text-xl font-semibold">Free</Text>
 
-        <Pressable className="rounded-md bg-red-500 p-5 px-8">
-          <Text className="text-lg font-bold text-white">Join and RSVP</Text>
-        </Pressable>
+        {attendace ? (
+          <Text className="font-bold text-green-500">You are attending</Text>
+        ) : (
+          <Pressable onPress={() => joinEvent()} className="rounded-md bg-red-500 p-5 px-8">
+            <Text className="text-lg font-bold text-white">Join and RSVP</Text>
+          </Pressable>
+        )}
       </View>
     </View>
   );
